2282f546f2c4e225547acd4564d39ee753d5fd4a
Victor Costa <littledeveloperman@gmail.com>
2022-07-29 22:30:17 -0300

    Projeto inicializado

---
+++ projeto/src/pages/index.js
@@ -0,0 +1,10 @@
+import {Button} from "@chakra-ui/react"
+
+export default function Home() {
+  return (
+    <>
+      <h1>Salve família</h1>
+      <Button colorScheme='blue'>Button</Button>
+    </>
+  );
+}
