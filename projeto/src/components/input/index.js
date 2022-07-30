5a1c93b1fa19128bb1bfc274794250997b564946
Victor Costa <littledeveloperman@gmail.com>
2022-07-29 23:34:57 -0300

    Front-end iniciado, tabela de clientes criada com sucesso

---
+++ projeto/src/components/input/index.js
@@ -0,0 +1,14 @@
+import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+
+const InputForm = ({ label, name, ...rest }) => {
+  return (
+    <>
+      <FormControl marginY="1rem">
+        <FormLabel>{label}</FormLabel>
+        <Input type="text" name={name} id={name} {...rest} />
+      </FormControl>
+    </>
+  );
+};
+
+export default InputForm;
--- projeto/src/pages/index.js
+++ projeto/src/pages/index.js
@@ -1,10 +1,72 @@
-import {Button} from "@chakra-ui/react"
+import { Button, VStack } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  Box,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  HStack,
+} from "@chakra-ui/react";
+
+import InputForm from '../components/input/';
 
 export default function Home() {
   return (
-    <>
-      <h1>Salve família</h1>
-      <Button colorScheme='blue'>Button</Button>
-    </>
+    <Box margin="4">
+      <Flex color="white" justifyContent="space-between">
+        <Text color="black" fontSize="2xl">
+          Lista de Clientes
+        </Text>
+
+        <Button colorScheme="blue">+</Button>
+      </Flex>
+
+      <VStack marginY="1rem">
+        <InputForm name="name" label="Nome"/>
+        <InputForm name="email" label="Email" type="email"/>
+        <Button fontSize="sm" alignSelf="flex-end" colorScheme="blue">
+          Cadastro
+        </Button>
+      </VStack>
+
+      <TableContainer>
+        <Table variant="simple">
+          <Thead bgColor="blue.500">
+            <Tr>
+              <Th textColor="white">Name</Th>
+              <Th textColor="white">Email</Th>
+              <Th textColor="white">Action</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>Victor FullStack Dev</Td>
+              <Td>vitinhobc.work@gmail.com</Td>
+              <Td>
+                <Flex justifyContent="space-between">
+                  <Button size="sm" fontSize="smaller" colorScheme="yellow">
+                    Editar
+                  </Button>
+                  <Button size="sm" fontSize="smaller" colorScheme="red">
+                    Remover
+                  </Button>
+                </Flex>
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
